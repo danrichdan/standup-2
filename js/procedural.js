4c9235cfab74713e189fbc02f38cb864f3d36834
Dan Riches <driches@ymail.com>
2020-06-10 23:49:39 -0700

    added a select box and some comments on what I want to do

---
+++ js/procedural.js
@@ -0,0 +1,9 @@
+// Tab Functionality
+
+// Select Box option determine the input field placeholder text
+
+// Form Field Validation
+// Get the form values on submit
+// Create lists based on that
+// Add Items to DOM, Delete Item, Update Item, Remove All Items
+// Add, Update and remove from LS
