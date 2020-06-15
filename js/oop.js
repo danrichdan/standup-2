55369f3152ad4a802e1f70be8c336c6bbb811206
Dan Riches <driches@ymail.com>
2020-06-14 23:19:24 -0700

    began a rewrite just using oop only

--- js/oop.js
+++ js/oop.js
@@ -1,11 +1,5 @@
-// Standup class
-class Standup {
-  constructor(category, item) {
-    this.category = category;
-    this.item = item;
-  }
-}
-
+//TABS
+// Tabs Class
 class Tabs {
   constructor() {
     this.tabs = document.querySelector(".tabs");
@@ -46,7 +40,82 @@ class Tabs {
 
 new Tabs();
 
+// BEGINNING OF APPLICATION
+// Standup class
+class Standup {
+  constructor(item, category) {
+    this.item = item;
+    this.category = category;
+  }
+}
+
+// Class for Forms
+class StandupForm {
+  constructor(formId, inputId) {
+    this.form = document.getElementById(formId);
+    this.standupInput = document.getElementById(inputId);
+    this.categoryForm = formId;
+
+    this.form.addEventListener("submit", this.getStandupItem.bind(this));
+  }
+
+  getStandupItem(e) {
+    e.preventDefault();
+    const standup = new Standup(this.standupInput.value, this.categoryForm);
+    standup.category = standup.category.substr(0, standup.category.length - 5);
+
+    // Instantiate List
+    const list = new List(`${standup.category}-list`);
+
+    // Call Add Item to List
+    list.addItemToList(standup.item);
+
+    // Add Delete Event Listener
+    list.removeItemFromList();
+
+    // Clear Field
+    this.standupInput.value = "";
+  }
+}
+
+// Instantiate the Forms
+const haveDoneForm = new StandupForm("have-done-form", "have-done-input");
+const workingOnForm = new StandupForm("working-on-form", "working-on-input");
+const blockerForm = new StandupForm("blockers-form", "blockers-input");
+
+class List {
+  constructor(list) {
+    this.list = document.getElementById(list);
+  }
+
+  // Add Item to List
+  addItemToList(standupItem) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = `
+      <i class="fas fa-edit"></i>
+      <i class="fas fa-times"></i>
+    `;
+    li.appendChild(document.createTextNode(standupItem));
+    li.appendChild(span);
+
+    // Append LI to List
+    this.list.appendChild(li);
+  }
+
+  // Remove Item from List
+  removeItemFromList() {
+    this.list.addEventListener("click", function (e) {
+      if (e.target.classList.contains("fa-times")) {
+        e.target.parentElement.parentElement.remove();
+      }
+    });
+  }
+
+  // Update Item
+  // Remove All Items
+}
+
 // Classes:
-// Form
-// List
+// Validation
 // Store
